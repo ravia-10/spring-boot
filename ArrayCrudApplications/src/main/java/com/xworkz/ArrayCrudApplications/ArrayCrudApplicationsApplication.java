25d6fb32db01ef073e28a20ee996521bb731496b
ravia-10 <ravia.workz@gmail.com>
2022-01-09 10:16:31 +0530

    spring boot

---
+++ ArrayCrudApplications/src/main/java/com/xworkz/ArrayCrudApplications/ArrayCrudApplicationsApplication.java
@@ -0,0 +1,13 @@
+package com.xworkz.ArrayCrudApplications;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArrayCrudApplicationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArrayCrudApplicationsApplication.class, args);
+		System.out.println("java developer server is started");
+	}
+
+}
